0def52efcd7c7a7c87e185a99d004eedeb9e7d3f
Hawk Weisman <hawk@meteorcodelabs.com>
2014-09-10 21:30:57 -0400

    Mostly complete Decaf parser with parser combinators!

--- scala/src/main/scala/parser/DecafParser.scala
+++ scala/src/main/scala/parser/DecafParser.scala
@@ -2,7 +2,39 @@ package decaf.parser
 import scala.util.parsing.combinators._
 
 object DecafParser extends RegexParsers {
-    def float: Parser[Float] = """[0-9]+\.[0-9]+?""".r ^^ {_.toFloat}
-    def int: Parser[Int] = """([0-9]+)|(0[xX][0-9a-fA-F]+)""".r ^^ {_.toInt}
-    def string: Parser[String] = """\"[^\n\"]\"""".r
+
+    val ident= """[a-zA-Z][a-zA-Z_]+""".r
+
+    def program = rep1(decl)
+    def decl = variableDecl
+             | functionDecl
+             | classDecl
+             | interfaceDecl
+    def variableDecl = variable ~ ";"
+    def variable = type ~ ident
+    def type = "int"
+             | "double"
+             | "bool"
+             | "string"
+             | ident
+             | type ~ "[]"
+    def functionDecl = Type ~ ident ~ "(" ~ formals ~ ")" ~ stmtBlock
+                     | "void" ~ ident ~ "(" ~ formals ~ ")" ~ stmtBlock
+    def formals = variable* | ""
+    def classDecl = "class" ~ ident ~ opt("extends" ~ ident) ~ opt("implements" ~ repsep(ident, ",")) "{" ~ field* ~ "}"
+    def field = variableDecl | functionDecl
+    def interfaceDecl = "interface" ~ ident ~ "{" ~ prototype* ~ "}"
+    def prototype = type ~ ident ~ "(" ~ formals ~ ")" ~ ";"
+                  | "void" ~ ident ~ "(" ~ formals ~ ")" ~ ";"
+    def stmtBlock = ~ "{" ~ variableDecl* ~ stmt* ~ "}"
+    def stmt = opt(expr) ~ ";"
+             | ifStmt
+             | whileStmt
+             | forStmt
+             | breakStmt
+             | returnStmt
+             | printStmt
+             | stmtBlock
+    def ifStmt = "if" ~ "(" ~ expr ~ ")" ~ stmt ~ opt("else" ~ stmt)
+
 }
