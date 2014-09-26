a843e3f6ff2e295fdd033db6dbba8b95fe0238f7
Hawk Weisman <hawk@meteorcodelabs.com>
2014-09-26 15:00:57 -0400

    Wipe repo and replace with Lab 2 files

--- y.tab.h
+++ y.tab.h
@@ -1,21 +1,19 @@
+/* A Bison parser, made by GNU Bison 3.0.2.  */
 
-/* A Bison parser, made by GNU Bison 2.4.1.  */
+/* Bison interface for Yacc-like parsers in C
 
-/* Skeleton interface for Bison's Yacc-like parsers in C
-   
-      Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
-   Free Software Foundation, Inc.
-   
+   Copyright (C) 1984, 1989-1990, 2000-2013 Free Software Foundation, Inc.
+
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
@@ -28,52 +26,60 @@
    special exception, which will cause the skeleton and the resulting
    Bison output files to be licensed under the GNU General Public
    License without this special exception.
-   
+
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
+#ifndef YY_YY_Y_TAB_H_INCLUDED
+# define YY_YY_Y_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 1
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
 
-/* Tokens.  */
+/* Token type.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
-   /* Put the tokens into the symbol table, so that GDB and other debuggers
-      know about them.  */
-   enum yytokentype {
-     T_Void = 258,
-     T_Bool = 259,
-     T_Int = 260,
-     T_Double = 261,
-     T_String = 262,
-     T_Class = 263,
-     T_LessEqual = 264,
-     T_GreaterEqual = 265,
-     T_Equal = 266,
-     T_NotEqual = 267,
-     T_Dims = 268,
-     T_And = 269,
-     T_Or = 270,
-     T_Null = 271,
-     T_Extends = 272,
-     T_This = 273,
-     T_Interface = 274,
-     T_Implements = 275,
-     T_While = 276,
-     T_For = 277,
-     T_If = 278,
-     T_Else = 279,
-     T_Return = 280,
-     T_Break = 281,
-     T_New = 282,
-     T_NewArray = 283,
-     T_Print = 284,
-     T_ReadInteger = 285,
-     T_ReadLine = 286,
-     T_Identifier = 287,
-     T_StringConstant = 288,
-     T_IntConstant = 289,
-     T_DoubleConstant = 290,
-     T_BoolConstant = 291
-   };
+  enum yytokentype
+  {
+    T_Void = 258,
+    T_Bool = 259,
+    T_Int = 260,
+    T_Double = 261,
+    T_String = 262,
+    T_Class = 263,
+    T_LessEqual = 264,
+    T_GreaterEqual = 265,
+    T_Equal = 266,
+    T_NotEqual = 267,
+    T_Dims = 268,
+    T_And = 269,
+    T_Or = 270,
+    T_Null = 271,
+    T_Extends = 272,
+    T_This = 273,
+    T_Interface = 274,
+    T_Implements = 275,
+    T_While = 276,
+    T_For = 277,
+    T_If = 278,
+    T_Else = 279,
+    T_Return = 280,
+    T_Break = 281,
+    T_New = 282,
+    T_NewArray = 283,
+    T_Print = 284,
+    T_ReadInteger = 285,
+    T_ReadLine = 286,
+    T_Identifier = 287,
+    T_StringConstant = 288,
+    T_IntConstant = 289,
+    T_DoubleConstant = 290,
+    T_BoolConstant = 291
+  };
 #endif
 /* Tokens.  */
 #define T_Void 258
@@ -111,15 +117,12 @@
 #define T_DoubleConstant 290
 #define T_BoolConstant 291
 
-
-
-
+/* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-typedef union YYSTYPE
+typedef union YYSTYPE YYSTYPE;
+union YYSTYPE
 {
-
-/* Line 1676 of yacc.c  */
-#line 41 "parser.y"
+#line 41 "parser.y" /* yacc.c:1909  */
 
     int integerConstant;
     bool boolConstant;
@@ -129,30 +132,29 @@ typedef union YYSTYPE
     Decl *decl;
     List<Decl*> *declList;
 
-
-
-/* Line 1676 of yacc.c  */
-#line 136 "y.tab.h"
-} YYSTYPE;
+#line 136 "y.tab.h" /* yacc.c:1909  */
+};
 # define YYSTYPE_IS_TRIVIAL 1
-# define yystype YYSTYPE /* obsolescent; will be withdrawn */
 # define YYSTYPE_IS_DECLARED 1
 #endif
 
-extern YYSTYPE yylval;
-
+/* Location type.  */
 #if ! defined YYLTYPE && ! defined YYLTYPE_IS_DECLARED
-typedef struct YYLTYPE
+typedef struct YYLTYPE YYLTYPE;
+struct YYLTYPE
 {
   int first_line;
   int first_column;
   int last_line;
   int last_column;
-} YYLTYPE;
-# define yyltype YYLTYPE /* obsolescent; will be withdrawn */
+};
 # define YYLTYPE_IS_DECLARED 1
 # define YYLTYPE_IS_TRIVIAL 1
 #endif
 
-extern YYLTYPE yylloc;
 
+extern YYSTYPE yylval;
+extern YYLTYPE yylloc;
+int yyparse (void);
+
+#endif /* !YY_YY_Y_TAB_H_INCLUDED  */
