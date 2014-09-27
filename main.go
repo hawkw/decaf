a136b8737e5063216c37a5d621cc55e37ce1d5c1
ArcticLight <mattrulz127@gmail.com>
2014-09-27 15:02:39 -0400

    Start tokenizer work

--- main.go
+++ main.go
@@ -1,6 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"bytes"
+	"regexp"
+)
+
+type token struct {
+	name String
+	value String
+}
+
+
+func tokenizer(in chan rune, out chan token) {
+	stack := new(bytes.Buffer);
+	continue := true;
+
+	for continue == true {
+		select {
+			case nextchar, open := <-in:
+				if(!open) {
+					continue = false;
+				}
+				else {
+					stack.WriteRune(nextchar)
+					switch thing := stack.Bytes() {
+						case regexp.Match("", thing):
+							out<-
+					}
+				}
+		}
+	}
+}
 
 func main() {
     fmt.Println("Hello decaf")
