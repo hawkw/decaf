34776aaed97b6b19a0a6d9454a7c8dd91fa6a3b9
Hawk Weisman <hawk@meteorcodelabs.com>
2014-09-29 13:01:33 -0400

    Move whitespace filtering to TestLexer

--- src/test/scala/LexerTests.scala
+++ src/test/scala/LexerTests.scala
@@ -8,9 +8,16 @@ import org.scalatest._
 import scala.io.Source
 import scala.util.parsing.input.CharArrayReader
 
+/**
+ * This is just a test [[decaf.frontend.DecafLexical D e c a f L e x i c a l]] that parses a whole program into a list of
+ * [[DecafTokens]]. Whitespace is ignored.
+ * @author Hawk Weisman
+ */
 object TestLexer extends DecafLexical {
 
-  def program: Parser[List[Any]] = rep(token | whitespaceChar)
+  def program: Parser[List[Any]] = rep(token | whitespaceChar) ^^ {
+    case tokens => tokens.filter(!_.isInstanceOf[Character])
+  }
 
 }
 
@@ -19,7 +26,7 @@ class LexerSamplesSpec extends FlatSpec with Matchers with DecafTokens {
   val program2 = Source.fromFile("build/resources/test/lab1-samples/program2.decaf").toArray
 
   "The Lexer" should "parse program2.decaf" in {
-    val tokens = TestLexer.program(new CharArrayReader(program2, 0)).get.filter(!_.isInstanceOf[Character])
+    val tokens = TestLexer.program(new CharArrayReader(program2, 0)).get
     tokens(0) shouldBe a[Keyword]
     tokens(0).toString should include(": int")
 
