c9283f7fc7c9f691b1d0c1a06fea91ab10705f31
Hawk Weisman <hawk@meteorcodelabs.com>
2014-10-01 16:17:55 -0400

    Some work on the syntactical analysis component
    It is definitely currently broken do not try to build pls kthnx, you will just be sad.

--- src/main/scala/decaf/frontend/DecafSyntactical.scala
+++ src/main/scala/decaf/frontend/DecafSyntactical.scala
@@ -19,10 +19,69 @@ import scala.util.parsing.combinator.syntactical.TokenParsers
  * @author Hawk Weisman
  *         Created by hawk on 9/28/14.
  */
-class DecafSyntactical extends TokenParsers with DecafAST {
-  type Tokens = DecafTokens
+class DecafSyntactical extends TokenParsers with DecafAST with DecafTokens {
+  override type Elem = DecafToken
+  type Tokens = DecafToken
   val lexical = new DecafLexical
 
-  def program = ???
-
+  def program: Program = rep(decl) ^^{case decls => new Program(decls)}
+  def decl = (
+    variableDecl
+    | functionDecl
+    //| classDecl
+    //| interfaceDecl
+    )
+  def functionDecl = (
+    typ ~ ident ~ Delimiter("(") ~ formals ~ Delimiter(")") ~ stmtBlock
+    | Keyword("void") ~ Delimiter("(") ~ formals ~ Delimiter(")") ~ stmtBlock
+    )
+  def formals = repsep(typ ~ ident, Delimiter(","))?
+  def stmtBlock = Delimiter("{") ~ (variableDecl | stmt).* ~ Delimiter("}") ^^{
+    case Delimiter("{") ~ stuff ~ Delimiter("}") => StmtBlock(stuff.filter(_.isInstanceOf[VarDecl]), stuff.filter(_.isInstanceOf[Stmt]))
+  }
+  def stmt =( expr.? ~ Delimiter(";")
+    | ifStmt
+    | whileStmt
+    | forStmt
+    | breakStmt
+    | Keyword("return") ~ expr.? ~ Delimiter(";") ^^{ case k ~ thing ~ _ => ReturnStmt(k.getPos, Some(thing.asInstanceOf[Expr]))}
+    )
+  def const = (
+    elem("intConst", _.isInstanceOf[IntConstant])
+    | elem("doubleConst", _.isInstanceOf[DoubleConstant])
+    | elem("BoolConst", _.isInstanceOf[BoolConstant])
+    | elem("StringConst", _.isInstanceOf[StringConstant])
+    | Keyword("null")
+    )^^{
+    case i: IntConstant => ASTIntConstant(i.getPos, i.value)
+    case d: DoubleConstant => ASTDoubleConstant(d.getPos, d.value)
+    case b: BoolConstant => ASTBoolConstant(b.getPos, b.value)
+    case s: StringConstant => ASTStringConstant(s.getPos, s.value)
+    case n: Keyword => ASTNullConstant(n.getPos)
+  }
+  def expr = (
+    lValue ~ Operator("=") ~ expr ^^{ case left ~ Operator("=") ~ right => AssignExpr(left.getPos, left, right)}
+    | const
+    | lValue
+    | Keyword("this")
+    | call
+    | Delimiter("(") ~ expr ~ Delimiter(")")
+    )
+  def lValue = (
+    ident
+    | expr ~ Delimiter(".") ~ ident ^^{case e ~ Delimiter(".") ~ i => FieldAccess(i.getPos, Some(e.asInstanceOf[Expr]), i.asInstanceOf[ASTIdentifier]) }
+    | expr ~ Delimiter("[") ~ expr ~ Delimiter("]") ^^{ case e }
+    )
+  def variableDecl = typ ~ ident ~ Delimiter(";") ^^{
+    case t ~ e => VarDecl(e.asInstanceOf[ASTIdentifier], t.asInstanceOf[Type])
+  }
+  def ident = elem("ident", _.isInstanceOf[Identifier]) ^^{ case i:Identifier => ASTIdentifier(Some(i.getPos), i.value)}
+  def typ = (_typ | _typ ~ Delimiter("[]") ^^{ case t ~ dims => ArrayType(Some(dims.getPos), t.asInstanceOf[Type])})
+  def _typ = (
+    Keyword("int") ^^{ case k => IntType() }
+    | Keyword("double") ^^{ case k => DoubleType() }
+    | Keyword("bool") ^^{ case k => BoolType() }
+    | Keyword("string") ^^{ case k => StringType() }
+    | ident
+    )
 }
