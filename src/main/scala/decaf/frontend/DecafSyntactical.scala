baa68fb855e38f4d71a80d2a790270b66bf441e9
Hawk Weisman <hawk@meteorcodelabs.com>
2014-10-02 10:46:28 -0400

    Additional work on the syntactical parser It now builds, but is incomplete (and therefore fails the tests)

--- src/main/scala/decaf/frontend/DecafSyntactical.scala
+++ src/main/scala/decaf/frontend/DecafSyntactical.scala
@@ -11,7 +11,7 @@
 
 package decaf.frontend
 
-import scala.util.parsing.combinator.syntactical.TokenParsers
+import scala.util.parsing.combinator.Parsers
 
 /**
  * Syntactical analyzer for the Decaf programming language
@@ -19,12 +19,12 @@ import scala.util.parsing.combinator.syntactical.TokenParsers
  * @author Hawk Weisman
  *         Created by hawk on 9/28/14.
  */
-class DecafSyntactical extends TokenParsers with DecafAST with DecafTokens {
+class DecafSyntactical extends Parsers with DecafAST with DecafTokens {
+  type Tokens <: DecafToken
   override type Elem = DecafToken
-  type Tokens = DecafToken
   val lexical = new DecafLexical
 
-  def program: Program = rep(decl) ^^{case decls => new Program(decls)}
+  def program: Parser[Program] = rep(decl) ^^{case decls => new Program(decls.asInstanceOf[List[Decl]])}
   def decl = (
     variableDecl
     | functionDecl
@@ -37,16 +37,18 @@ class DecafSyntactical extends TokenParsers with DecafAST with DecafTokens {
     )
   def formals = repsep(typ ~ ident, Delimiter(","))?
   def stmtBlock = Delimiter("{") ~ (variableDecl | stmt).* ~ Delimiter("}") ^^{
-    case Delimiter("{") ~ stuff ~ Delimiter("}") => StmtBlock(stuff.filter(_.isInstanceOf[VarDecl]), stuff.filter(_.isInstanceOf[Stmt]))
+    case Delimiter("{") ~ stuff ~ Delimiter("}") => StmtBlock(
+      stuff.filter(_.isInstanceOf[VarDecl]).asInstanceOf[List[VarDecl]],
+      stuff.filter(_.isInstanceOf[Stmt]).asInstanceOf[List[Stmt]])
   }
   def stmt =( expr.? ~ Delimiter(";")
-    | ifStmt
-    | whileStmt
-    | forStmt
-    | breakStmt
-    | Keyword("return") ~ expr.? ~ Delimiter(";") ^^{ case k ~ thing ~ _ => ReturnStmt(k.getPos, Some(thing.asInstanceOf[Expr]))}
+    //| ifStmt // TODO: Implement
+    //| whileStmt // TODO: Implement
+    //| forStmt // TODO: Implement
+    //| breakStmt // TODO: Implement
+    | Keyword("return") ~ expr.? ~ Delimiter(";") ^^{ case k ~ thing ~ _ => ReturnStmt(k.getPos, thing)}
     )
-  def const = (
+  def const: Parser[Expr] = (
     elem("intConst", _.isInstanceOf[IntConstant])
     | elem("doubleConst", _.isInstanceOf[DoubleConstant])
     | elem("BoolConst", _.isInstanceOf[BoolConstant])
@@ -59,29 +61,35 @@ class DecafSyntactical extends TokenParsers with DecafAST with DecafTokens {
     case s: StringConstant => ASTStringConstant(s.getPos, s.value)
     case n: Keyword => ASTNullConstant(n.getPos)
   }
-  def expr = (
+  def expr: Parser[Expr] = (
     lValue ~ Operator("=") ~ expr ^^{ case left ~ Operator("=") ~ right => AssignExpr(left.getPos, left, right)}
     | const
     | lValue
-    | Keyword("this")
-    | call
-    | Delimiter("(") ~ expr ~ Delimiter(")")
+    | Keyword("this") ^^{ case k => This(k.getPos) }
+    //| call // TODO: Implement
+    | Delimiter("(") ~ expr ~ Delimiter(")") ^^{ case Delimiter("(") ~ e ~ Delimiter(")") => e }
     )
-  def lValue = (
-    ident
-    | expr ~ Delimiter(".") ~ ident ^^{case e ~ Delimiter(".") ~ i => FieldAccess(i.getPos, Some(e.asInstanceOf[Expr]), i.asInstanceOf[ASTIdentifier]) }
-    | expr ~ Delimiter("[") ~ expr ~ Delimiter("]") ^^{ case e }
+  def lValue: Parser[LValue] = (
+    //ident ^^{ case i => i } //TODO: Figure out
+    expr.? ~ Delimiter(".") ~ ident ^^{case e ~ Delimiter(".") ~ i => FieldAccess(i.getPos, e, i)
+    }
+    | expr ~ Delimiter("[") ~ expr ~ Delimiter("]") ^^{ case first ~ Delimiter("[") ~ last ~ Delimiter("]") =>
+      ArrayAccess(first.getPos, first, last)
+      }
     )
   def variableDecl = typ ~ ident ~ Delimiter(";") ^^{
     case t ~ e => VarDecl(e.asInstanceOf[ASTIdentifier], t.asInstanceOf[Type])
   }
   def ident = elem("ident", _.isInstanceOf[Identifier]) ^^{ case i:Identifier => ASTIdentifier(Some(i.getPos), i.value)}
-  def typ = (_typ | _typ ~ Delimiter("[]") ^^{ case t ~ dims => ArrayType(Some(dims.getPos), t.asInstanceOf[Type])})
-  def _typ = (
-    Keyword("int") ^^{ case k => IntType() }
-    | Keyword("double") ^^{ case k => DoubleType() }
-    | Keyword("bool") ^^{ case k => BoolType() }
-    | Keyword("string") ^^{ case k => StringType() }
-    | ident
+  def typ: Parser[Type] = (
+    _typ
+    | _typ ~ Delimiter("[]") ^^{ case t ~ dims => ArrayType(Some(dims.getPos), t)}
+    )
+  def _typ: Parser[Type] = (
+    Keyword("int") ^^^ IntType()
+    | Keyword("double") ^^^ DoubleType()
+    | Keyword("bool") ^^^ BoolType()
+    | Keyword("string") ^^^ StringType()
+    | ident ^^{case i  => NamedType(i) }
     )
 }
--- src/test/scala/SyntacticalSpec.scala
+++ src/test/scala/SyntacticalSpec.scala
@@ -1,82 +1,85 @@
+import decaf.frontend.DecafLexical
 import org.scalatest.{Matchers, FlatSpec}
-import decaf.frontend.DecafParser
 
 import scala.io.Source
+import scala.util.parsing.input.CharArrayReader
 
 /**
  * Created by hawk on 10/1/14.
  */
 class ParserSpec extends FlatSpec with Matchers {
 
+  val target = new DecafLexical
+
   "The parser" should "correctly parse a simple program" in {
-    val source = Source fromFile "build/resources/test/lab2-samples/simple.decaf" mkString
+    val source = Source fromFile "build/resources/test/lab2-samples/simple.decaf" toArray
     val expected = Source fromFile "build/resources/test/lab2-samples/simple.out" mkString
-    val ast = DecafParser.parseAll(source).get
+    val ast = target.program(new CharArrayReader(source, 0)).get
 
     ast.toString should include (expected)
   }
 
   it should "handle control flow" in {
-      val source = Source fromFile "build/resources/test/lab2-samples/control.decaf" mkString
+      val source = Source fromFile "build/resources/test/lab2-samples/control.decaf" toArray
       val expected = Source fromFile "build/resources/test/lab2-samples/control.out" mkString
-      val ast = DecafParser.parseAll(source).get
+      val ast = target.program(new CharArrayReader(source, 0)).get
 
       ast.toString should include (expected)
   }
 
   it should "handle classes" in {
-    val source = Source fromFile "build/resources/test/lab2-samples/class.decaf" mkString
+    val source = Source fromFile "build/resources/test/lab2-samples/class.decaf" toArray
     val expected = Source fromFile "build/resources/test/lab2-samples/class.out" mkString
-    val ast = DecafParser.parseAll(source).get
+    val ast = target.program(new CharArrayReader(source, 0)).get
 
     ast.toString should include (expected)
   }
 
   it should "handle expressions" in {
-    val source = Source fromFile "build/resources/test/lab2-samples/expressions.decaf" mkString
+    val source = Source fromFile "build/resources/test/lab2-samples/expressions.decaf" toArray
     val expected = Source fromFile "build/resources/test/lab2-samples/expressions.out" mkString
-    val ast = DecafParser.parseAll(source).get
+    val ast = target.program(new CharArrayReader(source, 0)).get
 
     ast.toString should include (expected)
   }
 
   it should "handle functions" in {
-    val source = Source fromFile "build/resources/test/lab2-samples/functions.decaf" mkString
+    val source = Source fromFile "build/resources/test/lab2-samples/functions.decaf" toArray
     val expected = Source fromFile "build/resources/test/lab2-samples/functions.out" mkString
-    val ast = DecafParser.parseAll(source).get
+    val ast = target.program(new CharArrayReader(source, 0)).get
 
     ast.toString should include (expected)
   }
 
   it should "handle inheritance" in {
-    val source = Source fromFile "build/resources/test/lab2-samples/inheritance.decaf" mkString
+    val source = Source fromFile "build/resources/test/lab2-samples/inheritance.decaf" toArray
     val expected = Source fromFile "build/resources/test/lab2-samples/inheritance.out" mkString
-    val ast = DecafParser.parseAll(source).get
+    val ast = target.program(new CharArrayReader(source, 0)).get
 
     ast.toString should include (expected)
   }
 
   it should "handle the increment and decrement operators" in {
-    val source = Source fromFile "build/resources/test/lab2-samples/incrdecr.decaf" mkString
+    val source = Source fromFile "build/resources/test/lab2-samples/incrdecr.decaf" toArray
     val expected = Source fromFile "build/resources/test/lab2-samples/incrdecr.out" mkString
-    val ast = DecafParser.parseAll(source).get
+    val ast = target.program(new CharArrayReader(source, 0)).get
 
     ast.toString should include (expected)
   }
 
 
   it should "correctly parse a complex program" in {
-    val source = Source fromFile "build/resources/test/lab2-samples/switch.decaf" mkString
+    val source = Source fromFile "build/resources/test/lab2-samples/switch.decaf" toArray
     val expected = Source fromFile "build/resources/test/lab2-samples/switch.out" mkString
-    val ast = DecafParser.parseAll(source).get
+    val ast = target.program(new CharArrayReader(source, 0)).get
 
     ast.toString should include (expected)
   }
 
   it should "correctly parse a the matrix math program" in {
-    val source = Source fromFile "build/resources/test/lab2-samples/matrix.decaf" mkString
+    val source = Source fromFile "build/resources/test/lab2-samples/matrix.decaf" toArray
     val expected = Source fromFile "build/resources/test/lab2-samples/matrix.out" mkString
-    val ast = DecafParser.parseAll(source).get
+    val ast = target.program(new CharArrayReader(source, 0)).get
 
     ast.toString should include (expected)
   }
