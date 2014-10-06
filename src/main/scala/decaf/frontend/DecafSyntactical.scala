d62ca3779e899ddcbf07bf17cb0265da44e413ce
Max Clive <mattrulz127@gmail.com>
2014-10-06 07:42:49 -0400

    OH GOD ALL THE THINGS. (It's still pretty broken though)

--- src/main/scala/decaf/frontend/DecafSyntactical.scala
+++ src/main/scala/decaf/frontend/DecafSyntactical.scala
@@ -11,8 +11,7 @@
 
 package decaf.frontend
 
-import scala.collection.mutable
-import scala.util.parsing.combinator.Parsers
+import scala.util.parsing.combinator.{PackratParsers, Parsers}
 import scala.util.parsing.input.Reader
 
 /**
@@ -21,70 +20,187 @@ import scala.util.parsing.input.Reader
  * @author Hawk Weisman
  *         Created by hawk on 9/28/14.
  */
-class DecafSyntactical extends Parsers with DecafAST with DecafTokens {
+class DecafSyntactical extends Parsers with DecafAST with DecafTokens with PackratParsers {
   type Tokens <: DecafLexical
   override type Elem = DecafToken
   val lexical = new DecafLexical
 
-  implicit def dkeyword (k: Keyword): Parser[Elem] = acceptIf(_.name == k.name)("`"+k+"' expected but " + _ + " found")
-  implicit def ddelimiter (d: Delimiter): Parser[Elem] = acceptIf(_.name == d.name)("`"+d+"' expected but " + _ + " found")
+  implicit def dkeyword(k: Keyword): Parser[Elem] = acceptIf(_.name == k.name)("`" + k + "' expected but " + _ + " found")
+
+  implicit def ddelimiter(d: Delimiter): Parser[Elem] = acceptIf(_.name == d.name)("`" + d + "' expected but " + _ + " found")
 
   def parse(source: String): Option[Program] = {
     val scan = new lexical.DecafScanner(source).asInstanceOf[Reader[DecafToken]]
+
+    /**
+     * Some debuggery using Print.
+     */
+    /*var dscan = scan;
+    while(!dscan.atEnd) {
+      System.out.println(dscan.first)
+      dscan = dscan.rest
+    }*/
+
+
     phrase(program)(scan) match {
       case Success(result, _) => Some(result)
       case _ => None
     }
   }
-  def program: Parser[Program] = positioned(
-    decl.+ ^^{
+
+  lazy val program: PackratParser[Program] = positioned(
+    decl.+ ^^ {
       case decls => new Program(decls)
     })
-  def decl: Parser[Decl] = (
-    variableDecl
+
+  lazy val decl: PackratParser[Decl] = (
+    variableDecl ~ Delimiter(";") ^^ {case v ~ _ => v}
     | functionDecl
     //| classDecl
     //| interfaceDecl
     )
-  def formals = repsep(variableDecl, Delimiter(","))
-  def stmtBlock = Delimiter("{") ~ (variableDecl | (eexpr ~ Delimiter(";"))).* ~ Delimiter("}") ^^{
+
+  lazy val stmtBlock = Delimiter("{") ~ (variableDecl | stmt).* ~ Delimiter("}") ^^ {
     case Delimiter("{") ~ stuff ~ Delimiter("}") => StmtBlock(
-      List(),//stuff.filter(_.isInstanceOf[VarDecl]).asInstanceOf[List[VarDecl]],
-      List()//stuff.filter(_.isInstanceOf[Stmt]).asInstanceOf[List[Stmt]])
-      )
+      stuff.filter(_.isInstanceOf[VarDecl]).asInstanceOf[List[VarDecl]],
+      stuff.filter(_.isInstanceOf[Stmt]).asInstanceOf[List[Stmt]])
   }
 
-  //def bigstmt = stmt | variableDecl
+  lazy val returnType: PackratParser[Type] = (
+    Keyword("void") ^^^ VoidType()
+    | typ ^^{case t => t}
 
-  def stmt: Parser[Stmt] =(
-    eexpr.? ~ Delimiter(";") ^^{
-      case e ~ d => if (e.isDefined) {e.asInstanceOf[Stmt]} else {EmptyExpr(d.getPos)}
-    }
-    //| ifStmt
-    //| whileStmt
-    //| forStmt
-    //| breakStmt
-    //| Keyword("return") ~ expr.? ~ Delimiter(";") ^^{ case k ~ thing ~ _ => ReturnStmt(k.getPos, thing)}
     )
-  def ifStmt: Parser[Stmt] =
+
+  lazy val formals: PackratParser[List[VarDecl]] = (
+    Delimiter("(") ~ Delimiter(")") ^^^ List()
+    | Delimiter("(") ~ repsep(variableDecl, Delimiter(",")) ~ Delimiter(")") ^^ {case _ ~ stuff ~ _ => stuff}
+    )
+
+  lazy val fnargs: PackratParser[List[Expr]] = Delimiter("(") ~ repsep(expr, Delimiter(",")) ~ Delimiter(")") ^^{
+    case _ ~ stuff ~ _ => stuff
+  }
+
+  lazy val functionDecl: PackratParser[Decl] = returnType ~ ident ~ formals ~ stmtBlock ^^{
+    case rt ~ name ~ fs ~ body => FnDecl( name, rt, fs, body)
+  }
+
+  lazy val stmt: PackratParser[Stmt] =(
+    expr.? ~ Delimiter(";") ^^{
+      case e ~ d => if (e.isDefined) {e.get.asInstanceOf[Stmt]} else {EmptyExpr(d.getPos)}
+    }
+      | Keyword("Print") ~ Delimiter("(") ~ repsep(expr, Delimiter(",")) ~ Delimiter(")") ~ Delimiter(";") ^^ { case _ ~ _ ~ e ~ _ ~ _ => PrintStmt(e)}
+      | ifStmt
+      | whileStmt
+      | forStmt
+      | breakStmt
+      | Keyword("return") ~ expr.? ~ Delimiter(";") ^^{ case k ~ thing ~ _ => ReturnStmt(k.getPos, thing)}
+    )
+  lazy val breakStmt: PackratParser[Stmt] = Keyword("break") ~ Delimiter(";") ^^{case k ~ Delimiter(";") => BreakStmt(k.getPos)}
+  lazy val ifStmt: PackratParser[Stmt] =
     Keyword("if") ~ Delimiter("(") ~ expr ~ Delimiter(")") ~ stmt ~ opt(Keyword("else") ~> stmt) ^^{
       case Keyword("if") ~ Delimiter("(") ~ test ~ Delimiter(")") ~ testbody ~ elsebody => IfStmt(test,testbody,elsebody)
     }
-  def whileStmt: Parser[Stmt] = Keyword("while") ~ Delimiter("(") ~ expr ~ Delimiter(")") ~ stmt ^^{
+  lazy val whileStmt: PackratParser[Stmt] = Keyword("while") ~ Delimiter("(") ~ expr ~ Delimiter(")") ~ stmt ^^{
     case Keyword("while") ~ Delimiter("(") ~ test ~ Delimiter(")") ~ loopbody => WhileStmt(test,loopbody)
   }
-  def forStmt: Parser[Stmt] =
+  lazy val forStmt: PackratParser[Stmt] =
     Keyword("for") ~ Delimiter("(") ~ opt(expr) ~ Delimiter(";") ~ expr ~ Delimiter(";") ~ opt(expr) ~ Delimiter(")") ~ stmt ^^{
       case Keyword("for") ~ Delimiter("(") ~ i ~ Delimiter(";") ~ t ~ Delimiter(";") ~ s ~ Delimiter(")") ~ b =>
         ForStmt(i,t,s,b)
     }
-  def breakStmt: Parser[Stmt] = Keyword("break") ~ Delimiter(";") ^^{case k ~ Delimiter(";") => BreakStmt(k.getPos)}
-  def const: Parser[Expr] = (
+
+  lazy val expr: PackratParser[Expr] = (
+      Delimiter("(") ~ expr ~ Delimiter(")") ^^{ case Delimiter("(") ~ e ~ Delimiter(")") => e }
+      | const
+      | lValue
+      | call
+      | lValue ~ Operator ("=") ~ expr ^^ {case left ~ _ ~ right => AssignExpr(left.getPos, left, right)}
+      | lValue ~ Operator("=") ~ expr ^^{ case left ~ Operator("=") ~ right => AssignExpr(left.getPos, left, right)}
+      | Keyword("this") ^^{ case k => This(k.getPos) }
+      | expr ~ Operator("+") ~ expr ^^{
+         case left ~ Operator("+") ~ right => ArithmeticExpr(left.getPos, left, ASTOperator(left.getPos, "+"), right)
+       }
+      | expr ~ Operator("-") ~ expr ^^{
+       case left ~ Operator("-") ~ right => ArithmeticExpr(left.getPos, left, ASTOperator(left.getPos, "-"), right)
+      }
+        | expr ~ Operator("*") ~ expr ^^{
+        case left ~ Operator("*") ~ right => ArithmeticExpr(left.getPos, left, ASTOperator(left.getPos, "*"), right)
+      }
+        | expr ~ Operator("%") ~ expr ^^{
+        case left ~ Operator("%") ~ right => ArithmeticExpr(left.getPos, left, ASTOperator(left.getPos, "%"), right)
+      }
+        | expr ~ Operator("/") ~ expr ^^{
+        case left ~ Operator("/") ~ right => ArithmeticExpr(left.getPos, left, ASTOperator(left.getPos, "/"), right)
+      }
+        | expr ~ Operator(">") ~ expr ^^{
+        case left ~ Operator(">") ~ right => EqualityExpr(left.getPos, left, ASTOperator(left.getPos, ">"), right)
+      }
+        | expr ~ Operator(">=") ~ expr ^^{
+        case left ~ Operator(">=") ~ right => EqualityExpr(left.getPos, left, ASTOperator(left.getPos, ">="), right)
+      }
+        | expr ~ Operator("<=") ~ expr ^^{
+        case left ~ Operator("<=") ~ right => EqualityExpr(left.getPos, left, ASTOperator(left.getPos, "<="), right)
+      }
+        | expr ~ Operator("<") ~ expr ^^{
+        case left ~ Operator("<") ~ right => EqualityExpr(left.getPos, left, ASTOperator(left.getPos, "<"), right)
+      }
+        | expr ~ Operator("==") ~ expr ^^{
+        case left ~ Operator("==") ~ right => EqualityExpr(left.getPos, left, ASTOperator(left.getPos, "=="), right)
+      }
+        | expr ~ Operator("!=") ~ expr ^^{
+        case left ~ Operator("!=") ~ right => EqualityExpr(left.getPos, left, ASTOperator(left.getPos, "!="), right)
+      }
+        | expr ~ Operator("&&") ~ expr ^^{
+        case left ~ Operator("&&") ~ right => new LogicalExpr(left.getPos, left, ASTOperator(left.getPos, "&&"), right)
+      }
+        | expr ~ Operator("||") ~ expr ^^{
+        case left ~ Operator("||") ~ right => new LogicalExpr(left.getPos, left, ASTOperator(left.getPos, "||"), right)
+      }
+        | Operator("!") ~ expr ^^{
+        case Operator("!") ~ right => new LogicalExpr(right.getPos, ASTOperator(right.getPos, "!"), right)
+      }
+        | Keyword("ReadInteger") ~ Delimiter("(") ~ Delimiter(")") ^^{
+        case k ~ Delimiter("(") ~ Delimiter(")") => ReadIntegerExpr(k.getPos)
+      }
+        | Keyword("ReadLine") ~ Delimiter("(") ~ Delimiter(")") ^^{
+        case k ~ Delimiter("(") ~ Delimiter(")") => ReadLineExpr(k.getPos)
+      }
+        | Keyword("new") ~ ident ^^{
+        case Keyword("new") ~ i => NewExpr(i.getPos, NamedType(i))
+      }
+        | Keyword("NewArray") ~ Delimiter("(") ~ expr ~ Delimiter(",") ~ typ ~ Delimiter(")") ^^{
+        case Keyword("NewArray") ~ Delimiter("(") ~ e ~ Delimiter(",") ~ t ~ Delimiter(")") => NewArrayExpr(e.getPos,e,t)
+      }
+      | ident ^^{ case i => FieldAccess(i.getPos, None, i)}
+    )
+
+  lazy val lValue: PackratParser[LValue] = (
+
+    expr ~ Delimiter("[") ~ expr ~ Delimiter("]") ^^{
+      case first ~ Delimiter("[") ~ last ~ Delimiter("]") =>
+        ArrayAccess(first.getPos, first, last)
+    }
+    | expr ~ Delimiter(".") ~ ident ^^{case e ~ Delimiter(".") ~ i => FieldAccess(i.getPos, Some(e), i)}
+
+    )
+
+  lazy val call: PackratParser[Call] = (
+    expr ~ Delimiter(".") ~ ident ~ fnargs ^^{
+      case base ~ _ ~ field ~ args  =>
+        new Call(base.getPos, base, field, args)
+    }
+    | ident ~ fnargs ^^{
+      case field ~ args  => new Call(field.getPos, field, args)
+    }
+    )
+
+  lazy val const: PackratParser[Expr] = (
     elem("intConst", _.isInstanceOf[IntConstant])
-    | elem("doubleConst", _.isInstanceOf[DoubleConstant])
-    | elem("BoolConst", _.isInstanceOf[BoolConstant])
-    | elem("StringConst", _.isInstanceOf[StringConstant])
-    | Keyword("null")
+      | elem("doubleConst", _.isInstanceOf[DoubleConstant])
+      | elem("BoolConst", _.isInstanceOf[BoolConstant])
+      | elem("StringConst", _.isInstanceOf[StringConstant])
+      | Keyword("null")
     )^^{
     case i: IntConstant => ASTIntConstant(i.getPos, i.value)
     case d: DoubleConstant => ASTDoubleConstant(d.getPos, d.value)
@@ -93,82 +209,42 @@ class DecafSyntactical extends Parsers with DecafAST with DecafTokens {
     case n: Keyword => ASTNullConstant(n.getPos)
   }
 
-  def expr: Parser[Expr] = (
+  lazy val variableDecl: PackratParser[VarDecl] = typ ~ ident ^^ {
+    case t ~ e => VarDecl(e, t)
+  }
+
+  lazy val ident: PackratParser[ASTIdentifier] = elem("ident", _.isInstanceOf[Identifier]) ^^ {
+    case i: Identifier => ASTIdentifier(Some(i.getPos), i.value)
+  }
+
+  lazy val atyp: PackratParser[Type] = (
+      /* I don't know if Decaf supports multidimensional arrays,
+         but if you leave in this typ here at the top, we can do that.
+         Otherwise, take it out.
+       */
+      typ
+      | Keyword("int") ^^^ IntType()
+      | Keyword("double") ^^^ DoubleType()
+      | Keyword("bool") ^^^ BoolType()
+      | Keyword("string") ^^^ StringType()
+      | ident ^^ { case i => NamedType(i)}
+    )
+
+  lazy val typ: PackratParser[Type] = (
+      atyp ~ Delimiter("[]") ^^ { case t ~ dims => ArrayType(Some(dims.getPos), t)}
+      | atyp
+    )
+} /*
+
+
+  def eexpr: Parser[Expr] = (
       const
     )
 
-  def eexpr: Parser[ASTNode] = (
-    //lValue ~ Operator ("=") ~ expr ^^ {case left ~ _ ~ right => AssignExpr(left.getPos, left, right)}
-    //lValue ~ Operator("=") ~ expr ^^{ case left ~ Operator("=") ~ right => AssignExpr(left.getPos, left, right)}
-    expr
-    | lValue
-    //| call
-    | Keyword("this") ^^{ case k => This(k.getPos) }
-    | Delimiter("(") ~ eexpr ~ Delimiter(")") ^^{ case Delimiter("(") ~ e ~ Delimiter(")") => e }
-    | Keyword("Print") ~ Delimiter("(") ~ repsep(eexpr, Delimiter(",")) ~ Delimiter(")") ^^ { case _ ~ _ ~ e ~ _ => PrintStmt(e)}
-    //| expr ~ Operator("+") ~ expr ^^{
-    //  case left ~ Operator("+") ~ right => ArithmeticExpr(left.getPos, left, ASTOperator(left.getPos, "+"), right)
-    //}
-    //| expr ~ Operator("-") ~ expr ^^{
-    //  case left ~ Operator("-") ~ right => ArithmeticExpr(left.getPos, left, ASTOperator(left.getPos, "-"), right)
-    //}
-    /*| expr ~ Operator("*") ~ expr ^^{
-      case left ~ Operator("*") ~ right => ArithmeticExpr(left.getPos, left, ASTOperator(left.getPos, "*"), right)
-    }
-    | expr ~ Operator("%") ~ expr ^^{
-      case left ~ Operator("%") ~ right => ArithmeticExpr(left.getPos, left, ASTOperator(left.getPos, "%"), right)
-    }
-    | expr ~ Operator("/") ~ expr ^^{
-      case left ~ Operator("/") ~ right => ArithmeticExpr(left.getPos, left, ASTOperator(left.getPos, "/"), right)
-    }
-    | expr ~ Operator(">") ~ expr ^^{
-      case left ~ Operator(">") ~ right => EqualityExpr(left.getPos, left, ASTOperator(left.getPos, ">"), right)
-    }
-    | expr ~ Operator(">=") ~ expr ^^{
-      case left ~ Operator(">=") ~ right => EqualityExpr(left.getPos, left, ASTOperator(left.getPos, ">="), right)
-    }
-    | expr ~ Operator("<=") ~ expr ^^{
-      case left ~ Operator("<=") ~ right => EqualityExpr(left.getPos, left, ASTOperator(left.getPos, "<="), right)
-    }
-    | expr ~ Operator("<") ~ expr ^^{
-      case left ~ Operator("<") ~ right => EqualityExpr(left.getPos, left, ASTOperator(left.getPos, "<"), right)
-    }
-    | expr ~ Operator("==") ~ expr ^^{
-      case left ~ Operator("==") ~ right => EqualityExpr(left.getPos, left, ASTOperator(left.getPos, "=="), right)
-    }
-    | expr ~ Operator("!=") ~ expr ^^{
-      case left ~ Operator("!=") ~ right => EqualityExpr(left.getPos, left, ASTOperator(left.getPos, "!="), right)
-    }
-    | expr ~ Operator("&&") ~ expr ^^{
-      case left ~ Operator("&&") ~ right => new LogicalExpr(left.getPos, left, ASTOperator(left.getPos, "&&"), right)
-    }
-    | expr ~ Operator("||") ~ expr ^^{
-      case left ~ Operator("||") ~ right => new LogicalExpr(left.getPos, left, ASTOperator(left.getPos, "||"), right)
-    }
-    | Operator("!") ~ expr ^^{
-      case Operator("!") ~ right => new LogicalExpr(right.getPos, ASTOperator(right.getPos, "!"), right)
-    }
-    | Keyword("ReadInteger") ~ Delimiter("(") ~ Delimiter(")") ^^{
-      case k ~ Delimiter("(") ~ Delimiter(")") => ReadIntegerExpr(k.getPos)
-    }
-    | Keyword("ReadLine") ~ Delimiter("(") ~ Delimiter(")") ^^{
-      case k ~ Delimiter("(") ~ Delimiter(")") => ReadLineExpr(k.getPos)
-    }
-    | Keyword("new") ~ ident ^^{
-      case Keyword("new") ~ i => NewExpr(i.getPos, NamedType(i))
-    }
-    | Keyword("NewArray") ~ Delimiter("(") ~ expr ~ Delimiter(",") ~ typ ~ Delimiter(")") ^^{
-      case Keyword("NewArray") ~ Delimiter("(") ~ e ~ Delimiter(",") ~ t ~ Delimiter(")") => NewArrayExpr(e.getPos,e,t)
-    }*/
+  def expr: Parser[Expr] = (
+
     )
-  def lValue: Parser[LValue] = (
-    ident ^^{ case i => FieldAccess(i.getPos, None, i)}
-    | expr.? ~ Delimiter(".") ~ ident ^^{case e ~ Delimiter(".") ~ i => FieldAccess(i.getPos, e, i)
-    }
-    //| expr ~ Delimiter("[") ~ expr ~ Delimiter("]") ^^{ case first ~ Delimiter("[") ~ last ~ Delimiter("]") =>
-    //  ArrayAccess(first.getPos, first, last)
-    //  }
-    )
+
   def call: Parser[Call] = (
     ident ~  Delimiter("(") ~ repsep(expr, Delimiter(",")) ~ Delimiter(")") ^^{
       case field ~ Delimiter("(") ~ args ~ Delimiter(")")  => new Call(field.getPos, field, args)
@@ -178,9 +254,6 @@ class DecafSyntactical extends Parsers with DecafAST with DecafTokens {
         new Call(base.getPos, base, field, args)
     }
     )
-  def variableDecl: Parser[VarDecl] = typ ~ ident <~ Delimiter(";") ^^{
-    case t ~ e  => VarDecl(e, t)
-  }
   def functionDecl: Parser[Decl] = returnType ~ ident ~ Delimiter("(") ~ formals.? ~ Delimiter(")") ~ stmtBlock ^^{
       case rt ~ name ~ _ ~ fs ~ _ ~ body => FnDecl( name, rt, fs.getOrElse(Nil), body)
     }
@@ -191,23 +264,7 @@ class DecafSyntactical extends Parsers with DecafAST with DecafTokens {
   def extendPart: Parser[NamedType] = Keyword("extends") ~> className
   def implementsPart: Parser[List[NamedType]] = Keyword("implements") ~> repsep(className, Delimiter(","))
   def field: Parser[Decl] = (variableDecl | functionDecl)
-  def returnType: Parser[Type] = (
-    typ ^^{case t => t}
-    | Keyword("void") ^^^ VoidType()
-    )
+
   def className: Parser[NamedType] = ident ^^{ case i=> NamedType(i) }
-  def ident: Parser[ASTIdentifier] = elem("ident", _.isInstanceOf[Identifier]) ^^{
-    case i:Identifier => ASTIdentifier(Some(i.getPos), i.value)
-  }
-  def typ: Parser[Type] = (
-    _typ
-    | _typ ~ Delimiter("[]") ^^{ case t ~ dims => ArrayType(Some(dims.getPos), t)}
-    )
-  def _typ: Parser[Type] = (
-    Keyword("int") ^^^ IntType()
-    | Keyword("double") ^^^ DoubleType()
-    | Keyword("bool") ^^^ BoolType()
-    | Keyword("string") ^^^ StringType()
-    | ident ^^{case i  => NamedType(i) }
-    )
-}
+
+  */
--- src/test/scala/SyntacticalSpec.scala
+++ src/test/scala/SyntacticalSpec.scala
@@ -12,88 +12,94 @@ class ParserSpec extends FlatSpec with Matchers {
   val target = new DecafSyntactical
 
   "The parser" should "correctly parse a single statement" in {
-    val source= "sigv a;"
+    val source= "sigv[][] a;"
     val ast = target.parse(source).get
-    System.out.println(ast);
+    //System.out.println(ast);
   }
 
   it should "correctly parse a single function def" in {
     val source = "void main() { }"
     val ast = target.parse(source).get
+    //System.out.println(ast);
+  }
+
+  it should "correctly parse a complex statement" in {
+    val source = "void main() { a()[b]; }"
+    val ast = target.parse(source).get
     System.out.println(ast);
   }
 
-  "The parser" should "correctly parse a simple program" in {
-    val source = Source fromFile "build/resources/test/lab2-samples/simple.decaf" mkString
-    val expected = Source fromFile "build/resources/test/lab2-samples/simple.out" mkString
-    val ast = target.parse(source).get
-
-    ast.toString should include (expected)
-  }
-
-  it should "handle control flow" in {
-      val source = Source fromFile "build/resources/test/lab2-samples/control.decaf" mkString
-      val expected = Source fromFile "build/resources/test/lab2-samples/control.out" mkString
+    "The parser" should "correctly parse a simple program" in {
+      val source = Source fromFile "build/resources/test/lab2-samples/simple.decaf" mkString
+      val expected = Source fromFile "build/resources/test/lab2-samples/simple.out" mkString
       val ast = target.parse(source).get
 
       ast.toString should include (expected)
-  }
+    }
 
-  it should "handle classes" in {
-    val source = Source fromFile "build/resources/test/lab2-samples/class.decaf" mkString
-    val expected = Source fromFile "build/resources/test/lab2-samples/class.out" mkString
-    val ast = target.parse(source).get
+    it should "handle control flow" in {
+        val source = Source fromFile "build/resources/test/lab2-samples/control.decaf" mkString
+        val expected = Source fromFile "build/resources/test/lab2-samples/control.out" mkString
+        val ast = target.parse(source).get
 
-    ast.toString should include (expected)
-  }
+        ast.toString should include (expected)
+    }
 
-  it should "handle expressions" in {
-    val source = Source fromFile "build/resources/test/lab2-samples/expressions.decaf" mkString
-    val expected = Source fromFile "build/resources/test/lab2-samples/expressions.out" mkString
-    val ast = target.parse(source).get
+    it should "handle classes" in {
+      val source = Source fromFile "build/resources/test/lab2-samples/class.decaf" mkString
+      val expected = Source fromFile "build/resources/test/lab2-samples/class.out" mkString
+      val ast = target.parse(source).get
 
-    ast.toString should include (expected)
-  }
+      ast.toString should include (expected)
+    }
 
-  it should "handle functions" in {
-    val source = Source fromFile "build/resources/test/lab2-samples/functions.decaf" mkString
-    val expected = Source fromFile "build/resources/test/lab2-samples/functions.out" mkString
-    val ast = target.parse(source).get
+    it should "handle expressions" in {
+      val source = Source fromFile "build/resources/test/lab2-samples/expressions.decaf" mkString
+      val expected = Source fromFile "build/resources/test/lab2-samples/expressions.out" mkString
+      val ast = target.parse(source).get
 
-    ast.toString should include (expected)
-  }
+      ast.toString should include (expected)
+    }
 
-  it should "handle inheritance" in {
-    val source = Source fromFile "build/resources/test/lab2-samples/inheritance.decaf" mkString
-    val expected = Source fromFile "build/resources/test/lab2-samples/inheritance.out" mkString
-    val ast = target.parse(source).get
+    it should "handle functions" in {
+      val source = Source fromFile "build/resources/test/lab2-samples/functions.decaf" mkString
+      val expected = Source fromFile "build/resources/test/lab2-samples/functions.out" mkString
+      val ast = target.parse(source).get
 
-    ast.toString should include (expected)
-  }
+      ast.toString should include (expected)
+    }
 
-  it should "handle the increment and decrement operators" in {
-    val source = Source fromFile "build/resources/test/lab2-samples/incrdecr.decaf" mkString
-    val expected = Source fromFile "build/resources/test/lab2-samples/incrdecr.out" mkString
-    val ast = target.parse(source).get
+    it should "handle inheritance" in {
+      val source = Source fromFile "build/resources/test/lab2-samples/inheritance.decaf" mkString
+      val expected = Source fromFile "build/resources/test/lab2-samples/inheritance.out" mkString
+      val ast = target.parse(source).get
 
-    ast.toString should include (expected)
-  }
+      ast.toString should include (expected)
+    }
+
+    it should "handle the increment and decrement operators" in {
+      val source = Source fromFile "build/resources/test/lab2-samples/incrdecr.decaf" mkString
+      val expected = Source fromFile "build/resources/test/lab2-samples/incrdecr.out" mkString
+      val ast = target.parse(source).get
+
+      ast.toString should include (expected)
+    }
 
 
-  it should "correctly parse a complex program" in {
-    val source = Source fromFile "build/resources/test/lab2-samples/switch.decaf" mkString
-    val expected = Source fromFile "build/resources/test/lab2-samples/switch.out" mkString
-    val ast = target.parse(source).get
+    it should "correctly parse a complex program" in {
+      val source = Source fromFile "build/resources/test/lab2-samples/switch.decaf" mkString
+      val expected = Source fromFile "build/resources/test/lab2-samples/switch.out" mkString
+      val ast = target.parse(source).get
 
-    ast.toString should include (expected)
-  }
+      ast.toString should include (expected)
+    }
 
-  it should "correctly parse a the matrix math program" in {
-    val source = Source fromFile "build/resources/test/lab2-samples/matrix.decaf" mkString
-    val expected = Source fromFile "build/resources/test/lab2-samples/matrix.out" mkString
-    val ast = target.parse(source).get
+    it should "correctly parse a the matrix math program" in {
+      val source = Source fromFile "build/resources/test/lab2-samples/matrix.decaf" mkString
+      val expected = Source fromFile "build/resources/test/lab2-samples/matrix.out" mkString
+      val ast = target.parse(source).get
 
-    ast.toString should include (expected)
-  }
+      ast.toString should include (expected)
+    }
 
 }
