e091058d86b6f87325427aa1f8203db65ee45f9e
Max Clive <mattrulz127@gmail.com>
2014-10-06 05:20:49 -0400

    Seriously Mangle Hawk's Code, towards actually producing a working parser.

--- src/main/scala/decaf/frontend/DecafSyntactical.scala
+++ src/main/scala/decaf/frontend/DecafSyntactical.scala
@@ -11,6 +11,7 @@
 
 package decaf.frontend
 
+import scala.collection.mutable
 import scala.util.parsing.combinator.Parsers
 import scala.util.parsing.input.Reader
 
@@ -42,24 +43,28 @@ class DecafSyntactical extends Parsers with DecafAST with DecafTokens {
   def decl: Parser[Decl] = (
     variableDecl
     | functionDecl
-    | classDecl
+    //| classDecl
     //| interfaceDecl
     )
   def formals = repsep(variableDecl, Delimiter(","))
-  def stmtBlock = Delimiter("{") ~ (variableDecl | stmt).* ~ Delimiter("}") ^^{
+  def stmtBlock = Delimiter("{") ~ (variableDecl | (eexpr ~ Delimiter(";"))).* ~ Delimiter("}") ^^{
     case Delimiter("{") ~ stuff ~ Delimiter("}") => StmtBlock(
-      stuff.filter(_.isInstanceOf[VarDecl]).asInstanceOf[List[VarDecl]],
-      stuff.filter(_.isInstanceOf[Stmt]).asInstanceOf[List[Stmt]])
+      List(),//stuff.filter(_.isInstanceOf[VarDecl]).asInstanceOf[List[VarDecl]],
+      List()//stuff.filter(_.isInstanceOf[Stmt]).asInstanceOf[List[Stmt]])
+      )
   }
+
+  //def bigstmt = stmt | variableDecl
+
   def stmt: Parser[Stmt] =(
-    expr.? ~ Delimiter(";") ^^{
+    eexpr.? ~ Delimiter(";") ^^{
       case e ~ d => if (e.isDefined) {e.asInstanceOf[Stmt]} else {EmptyExpr(d.getPos)}
     }
-    | ifStmt
-    | whileStmt
-    | forStmt
-    | breakStmt
-    | Keyword("return") ~ expr.? ~ Delimiter(";") ^^{ case k ~ thing ~ _ => ReturnStmt(k.getPos, thing)}
+    //| ifStmt
+    //| whileStmt
+    //| forStmt
+    //| breakStmt
+    //| Keyword("return") ~ expr.? ~ Delimiter(";") ^^{ case k ~ thing ~ _ => ReturnStmt(k.getPos, thing)}
     )
   def ifStmt: Parser[Stmt] =
     Keyword("if") ~ Delimiter("(") ~ expr ~ Delimiter(")") ~ stmt ~ opt(Keyword("else") ~> stmt) ^^{
@@ -87,20 +92,27 @@ class DecafSyntactical extends Parsers with DecafAST with DecafTokens {
     case s: StringConstant => ASTStringConstant(s.getPos, s.value)
     case n: Keyword => ASTNullConstant(n.getPos)
   }
+
   def expr: Parser[Expr] = (
-    lValue ~ Operator("=") ~ expr ^^{ case left ~ Operator("=") ~ right => AssignExpr(left.getPos, left, right)}
-    | const
+      const
+    )
+
+  def eexpr: Parser[ASTNode] = (
+    //lValue ~ Operator ("=") ~ expr ^^ {case left ~ _ ~ right => AssignExpr(left.getPos, left, right)}
+    //lValue ~ Operator("=") ~ expr ^^{ case left ~ Operator("=") ~ right => AssignExpr(left.getPos, left, right)}
+    expr
     | lValue
+    //| call
     | Keyword("this") ^^{ case k => This(k.getPos) }
-    | call
-    | Delimiter("(") ~ expr ~ Delimiter(")") ^^{ case Delimiter("(") ~ e ~ Delimiter(")") => e }
-    | expr ~ Operator("+") ~ expr ^^{
-      case left ~ Operator("+") ~ right => ArithmeticExpr(left.getPos, left, ASTOperator(left.getPos, "+"), right)
-    }
-    | expr ~ Operator("-") ~ expr ^^{
-      case left ~ Operator("-") ~ right => ArithmeticExpr(left.getPos, left, ASTOperator(left.getPos, "-"), right)
-    }
-    | expr ~ Operator("*") ~ expr ^^{
+    | Delimiter("(") ~ eexpr ~ Delimiter(")") ^^{ case Delimiter("(") ~ e ~ Delimiter(")") => e }
+    | Keyword("Print") ~ Delimiter("(") ~ repsep(eexpr, Delimiter(",")) ~ Delimiter(")") ^^ { case _ ~ _ ~ e ~ _ => PrintStmt(e)}
+    //| expr ~ Operator("+") ~ expr ^^{
+    //  case left ~ Operator("+") ~ right => ArithmeticExpr(left.getPos, left, ASTOperator(left.getPos, "+"), right)
+    //}
+    //| expr ~ Operator("-") ~ expr ^^{
+    //  case left ~ Operator("-") ~ right => ArithmeticExpr(left.getPos, left, ASTOperator(left.getPos, "-"), right)
+    //}
+    /*| expr ~ Operator("*") ~ expr ^^{
       case left ~ Operator("*") ~ right => ArithmeticExpr(left.getPos, left, ASTOperator(left.getPos, "*"), right)
     }
     | expr ~ Operator("%") ~ expr ^^{
@@ -147,15 +159,15 @@ class DecafSyntactical extends Parsers with DecafAST with DecafTokens {
     }
     | Keyword("NewArray") ~ Delimiter("(") ~ expr ~ Delimiter(",") ~ typ ~ Delimiter(")") ^^{
       case Keyword("NewArray") ~ Delimiter("(") ~ e ~ Delimiter(",") ~ t ~ Delimiter(")") => NewArrayExpr(e.getPos,e,t)
-    }
+    }*/
     )
   def lValue: Parser[LValue] = (
     ident ^^{ case i => FieldAccess(i.getPos, None, i)}
     | expr.? ~ Delimiter(".") ~ ident ^^{case e ~ Delimiter(".") ~ i => FieldAccess(i.getPos, e, i)
     }
-    | expr ~ Delimiter("[") ~ expr ~ Delimiter("]") ^^{ case first ~ Delimiter("[") ~ last ~ Delimiter("]") =>
-      ArrayAccess(first.getPos, first, last)
-      }
+    //| expr ~ Delimiter("[") ~ expr ~ Delimiter("]") ^^{ case first ~ Delimiter("[") ~ last ~ Delimiter("]") =>
+    //  ArrayAccess(first.getPos, first, last)
+    //  }
     )
   def call: Parser[Call] = (
     ident ~  Delimiter("(") ~ repsep(expr, Delimiter(",")) ~ Delimiter(")") ^^{
@@ -170,7 +182,7 @@ class DecafSyntactical extends Parsers with DecafAST with DecafTokens {
     case t ~ e  => VarDecl(e, t)
   }
   def functionDecl: Parser[Decl] = returnType ~ ident ~ Delimiter("(") ~ formals.? ~ Delimiter(")") ~ stmtBlock ^^{
-      case rt ~ name ~ Delimiter("(") ~ fs ~ Delimiter(")") ~ body => FnDecl( name, rt, fs.getOrElse(Nil), body)
+      case rt ~ name ~ _ ~ fs ~ _ ~ body => FnDecl( name, rt, fs.getOrElse(Nil), body)
     }
   def classDecl: Parser[Decl] =
     Keyword("class") ~> ident ~ extendPart.? ~ implementsPart ~ Delimiter("{") ~ rep(field) ~ Delimiter("}") ^^{
