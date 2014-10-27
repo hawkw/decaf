6472b1f04906df7e01baf1126c0e33581b0333b0
Hawk Weisman <hawk@meteorcodelabs.com>
2014-10-27 16:17:51 -0400

    fix dcc

--- src/main/scala/decaf/Compiler.scala
+++ src/main/scala/decaf/Compiler.scala
@@ -1,8 +1,6 @@
 package decaf
 import decaf.frontend.DecafSyntactical
 import scala.io.Source
-import concurrent.Future
-import concurrent.ExecutionContext.Implicits.global
 
 /**
  * Quick driver class for command-line invocation of the compiler.
@@ -12,15 +10,13 @@ import concurrent.ExecutionContext.Implicits.global
  */
 object Compiler extends App {
   val parser = new DecafSyntactical
-  def parseFromFile(path)
-
-   args.length match {
-     case 0 => println("Please enter a Decaf source code file to compile.")
-     case 1 =>
-       val source = Source.fromFile(args(0)).mkString
-       val ast = parser.parse(source)
-       println(ast)
-     case _ => println("Too many arguments!")
-   }
+  args.length match {
+    case 0 => println("Please enter a Decaf source code file to compile.")
+    case 1 =>
+      val source = Source.fromFile(args(0)).mkString
+      val ast = parser.parse(source)
+      println(ast)
+    case _ => println("Too many arguments!")
+  }
 
 }
