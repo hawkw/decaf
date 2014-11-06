e80cce82d81674878edb979787c0e77570716bdf
Hawk Weisman <hawk@meteorcodelabs.com>
2014-11-06 17:58:10 -0500

    Now with a test for checkTypes based on the samples I think this is how we should test for error-catching?

--- src/test/scala/SemanticSpec.scala
+++ src/test/scala/SemanticSpec.scala
@@ -1,8 +1,22 @@
+import decaf.frontend.{DecafSemantic, DecafSyntactical, ConflictingDeclException}
 import org.scalatest.{FlatSpec, Matchers}
+
+import scala.io.Source
 
 /**
  * Created by hawk on 11/6/14.
  */
-class SemanticSpec extends FlatSpec with Matchers {
+class SemanticCheckpointSpec extends FlatSpec with Matchers {
+
+  val parser = new DecafSyntactical
+  val target = DecafSemantic
+
+  "checkTypes()" should "detect the conflicting declaration in bad1.decaf" in {
+    val source = Source fromFile "build/resources/test/lab3-samples/samples-checkpoint/bad1.decaf" mkString
+    val (scopes, errs) = target analyze (parser parse source)
+    errs should have length 1
+    errs(0) shouldBe a [ConflictingDeclException]
+    errs(0).getMessage should include ("Declaration of 'b' here conflicts")
+  }
 
 }
