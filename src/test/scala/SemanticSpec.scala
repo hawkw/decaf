5559914f583e4efaed2b59818a108667e9c03823
Hawk Weisman <hawk@meteorcodelabs.com>
2014-11-06 20:06:18 -0500

    Add a couple more tests

--- src/test/scala/SemanticSpec.scala
+++ src/test/scala/SemanticSpec.scala
@@ -1,4 +1,4 @@
-import decaf.frontend.{DecafSemantic, DecafSyntactical, ConflictingDeclException}
+import decaf.frontend.{UndeclaredTypeException, DecafSemantic, DecafSyntactical, ConflictingDeclException}
 import org.scalatest.{FlatSpec, Matchers}
 
 import scala.io.Source
@@ -17,6 +17,23 @@ class SemanticCheckpointSpec extends FlatSpec with Matchers {
     errs should have length 1
     errs(0) shouldBe a [ConflictingDeclException]
     errs(0).getMessage should include ("Declaration of 'b' here conflicts")
+  }
+
+  it should "detect the conflicting declaration in bad2.decaf" in {
+    val source = Source fromFile "build/resources/test/lab3-samples/samples-checkpoint/bad2.decaf" mkString
+    val (scopes, errs) = target analyze (parser parse source)
+    errs should have length 1
+    errs(0) shouldBe a [ConflictingDeclException]
+    errs(0).getMessage should include ("Declaration of 'Rutabaga' here conflicts")
+  }
+
+  it should "detect the undeclared types in bad3.decaf" in {
+    val source = Source fromFile "build/resources/test/lab3-samples/samples-checkpoint/bad3.decaf" mkString
+    val (scopes, errs) = target analyze (parser parse source)
+    errs should have length 3
+    errs(0) shouldBe a [UndeclaredTypeException]
+    errs(1) shouldBe a [UndeclaredTypeException]
+    errs(2) shouldBe a [UndeclaredTypeException]
   }
 
 }
