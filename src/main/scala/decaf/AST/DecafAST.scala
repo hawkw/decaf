c1aeb5c68f518e6bbeecd94fbcdde4c7217b81b4
Hawk Weisman <hawk@meteorcodelabs.com>
2014-11-16 13:15:04 -0500

    Fix incorrect error string in FieldAccess.typeof()

--- src/main/scala/decaf/AST/DecafAST.scala
+++ src/main/scala/decaf/AST/DecafAST.scala
@@ -467,13 +467,13 @@ import scala.util.parsing.input.{Position, Positional}
                 s"\n*** EXTREMELY BAD PROBLEM occurs on line ${loc.line}" +
                   s"\n*** this should not happen ever,  please contact the decaf implementors and I am sorry" +
                   s"\n*** code:\n${loc.longString}")
-            case ClassAnnotation(_, _, _, classScope, where) => classScope.table.get(field.name) match {
+            case ClassAnnotation(what, _, _, classScope, where) => classScope.table.get(field.name) match {
               case Some(thing) => thing match {
                 case VariableAnnotation(t, _) => t
                 case MethodAnnotation(_, _, _) => new ErrorType("*** Attempt to field access a method", where)
                 case ClassAnnotation(_, _, _, _, _) => new ErrorType("*** Attempt to field access a class", where)
               }
-              case None => UndeclaredType("*** No declaration found for variable '" + field.name + "'.", where)
+              case None => UndeclaredType(s"*** ${what.typeName} has no such field '${field.name}'", where)
             }
           }
         } else {
