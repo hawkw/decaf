a09c482ee34c568bd604f949eeca42cb34a1bfbd
Hawk Weisman <hawk@meteorcodelabs.com>
2014-12-13 13:21:48 -0500

    Fix some issues where backend didn't get identifiers

--- src/main/scala/decaf/backend/JasminBackend.scala
+++ src/main/scala/decaf/backend/JasminBackend.scala
@@ -2,6 +2,7 @@ package decaf.backend
 
 import decaf.AST._
 
+import scala.annotation.tailrec
 import scala.collection.mutable
 
 /**
@@ -9,7 +10,7 @@ import scala.collection.mutable
  */
 object JasminBackend {
 
-  def compile(program: Program): String = ???
+  def compile(program: Program): String = s"${makeHeader(program.getName)}\n${emit(program)}"
 
   private def makeHeader(name: String, sup: String="java/lang/Object") = s".class public $name\n.super $sup\n" + makeInitializer(sup)
   private def makeInitializer(sup: String) = sup match {
@@ -23,25 +24,29 @@ object JasminBackend {
     case _ => ??? //TODO: this is where classes would actually happen
   }
 
-  private def getFnScope(node: ASTNode): String = node match {
+  @tailrec private def getFnScope(node: ASTNode): String = node match {
     case _: FnDecl => node.state.get.boundName
     case _ => getFnScope(node.parent)
   }
-  private def getNextVar(localVars: mutable.Map[String,Int]) = localVars.unzip._2.max + 1
+
+  private def getNextVar(localVars: mutable.Map[String,Int]) = localVars.unzip._2 match {
+    case it if it isEmpty => 1
+    case it => it.max + 1
+  }
   private def emit(node: ASTNode, localVars: mutable.Map[String, Int] = mutable.Map[String, Int]()): String = node match {
     case Program(decls, _) => decls.foldLeft("")((acc, decl) => acc + emit(decl))
     case VarDecl(n, t) => node.parent match {
       case _: Program => s".field public $n ${emit(t)}\n"
       case _: StmtBlock => val fnName = getFnScope(node)
         localVars += (n.name -> getNextVar(localVars))
-        s".var ${getNextVar(localVars)} is $n $t from Begin$fnName to End$fnName"
+        s".var ${getNextVar(localVars)} is ${n.name} ${t.typeName} from Begin$fnName to End$fnName"
     } // TODO:
-    case FnDecl(name, rt, args, Some(code)) => s".method public static $name(${args.map(emit(_)).mkString(";")})" +
+    case FnDecl(ASTIdentifier(_,name), rt, args, Some(code)) => s".method public static $name(${args.map(emit(_)).mkString(";")})" +
       s"\n.limit locals ${code.decls.length}\n${emit(code)}\nEnd${node.state.get.boundName}\n.end method\n"
-    case StmtBlock(declarations, code, _) => {
+    case StmtBlock(declarations, code, _) =>
       val vars = mutable.Map[String, Int]() // not very functional but w/e
       declarations.map(emit(_, vars)).mkString("\n") + code.map(emit(_, vars)).mkString("\n")
-    } // todo: finish
+     // todo: finish
     case FnDecl(name, rt, args, None) => ??? //NYI: interfaces aren't implemented
     case _ => println(s"ignored $node"); ""
 
