b189cb3b1cd1fe3993eaeed647dd3e1ed135e9c7
Hawk Weisman <hawk@meteorcodelabs.com>
2014-12-13 23:42:37 -0500

    Always pop hex consts onto stack

--- src/main/scala/decaf/backend/JasminBackend.scala
+++ src/main/scala/decaf/backend/JasminBackend.scala
@@ -102,18 +102,18 @@ object JasminBackend {
                   val lab = rand.nextInt()        // to put a bool on the stack
                                                   // (hey, that rhymes!)
                   ("\t" * (tabLevel + 1))   + s"if_icmpeq\tCmp$lab\n" +
-                    ("\t" * (tabLevel + 1)) + s"ldc\t\t0\n" +
+                    ("\t" * (tabLevel + 1)) + s"ldc\t\t0x0\n" +
                     ("\t" * (tabLevel + 1)) + s"goto\t\tCmpDone$lab\n" +
                     ("\t" * tabLevel)       + s"Cmp$lab:\n" +
-                    ("\t" * (tabLevel + 1)) + s"ldc\t\t1\n" +
+                    ("\t" * (tabLevel + 1)) + s"ldc\t\t0x1\n" +
                     ("\t" * tabLevel)       + s"CmpDone$lab:\n"
                 case _: StringType | _: NamedType =>
                   val lab = rand.nextInt()
                   ("\t" * (tabLevel + 1))   + s"if_acmpeq\tCmp$lab\n" +
-                    ("\t" * (tabLevel + 1)) + s"ldc\t\t0\n" +
+                    ("\t" * (tabLevel + 1)) + s"ldc\t\t0x0\n" +
                     ("\t" * (tabLevel + 1)) + s"goto\tCmpDone$lab\n" +
                     ("\t" * tabLevel)       + s"Cmp$lab:\n" +
-                    ("\t" * (tabLevel + 1)) + s"ldc\t\t1\n" +
+                    ("\t" * (tabLevel + 1)) + s"ldc\t\t0x1\n" +
                     ("\t" * tabLevel)       + s"CmpDone$lab:\n"
                 case _: DoubleType => ("\t" * (tabLevel + 1)) + s"dcmpg\n"
 
@@ -124,24 +124,24 @@ object JasminBackend {
                 case _: IntType | _: BoolType =>
                   val lab = rand.nextInt()
                   ("\t" * (tabLevel + 1))   + s"if_icmpne\tCmp$lab\n" +
-                    ("\t" * (tabLevel + 1)) + s"ldc\t\t0\n" +
+                    ("\t" * (tabLevel + 1)) + s"ldc\t\t0x0\n" +
                     ("\t" * (tabLevel + 1)) + s"goto\t\tCmpDone$lab\n" +
                     ("\t" * tabLevel)       + s"Cmp$lab:\n" +
-                    ("\t" * (tabLevel + 1)) + s"ldc\t\t1\n" +
+                    ("\t" * (tabLevel + 1)) + s"ldc\t\t0x1\n" +
                     ("\t" * tabLevel)       + s"CmpDone$lab:\n"
                 case _: StringType | _: NamedType =>
                   val lab = rand.nextInt()
                   ("\t" * (tabLevel + 1))   + s"if_acmpeq\t\tCmp$lab\n" +
-                    ("\t" * (tabLevel + 1)) + s"ldc\t\t0\n" +
+                    ("\t" * (tabLevel + 1)) + s"ldc\t\t0x0\n" +
                     ("\t" * (tabLevel + 1)) + s"goto\t\tCmpDone$lab\n" +
                     ("\t" * tabLevel)       + s"Cmp$lab:\n" +
-                    ("\t" * (tabLevel + 1)) + s"ldc\t\t1\n" +
+                    ("\t" * (tabLevel + 1)) + s"ldc\t\t0x1\n" +
                     ("\t" * tabLevel)      + s"CmpDone$lab:\n"
                 case _: DoubleType => ("\t" * (tabLevel + 1)) + s"dcmpg\n"
               })
             })
-      case ASTIntConstant(_, value) => ("\t" * tabLevel) + s"ldc\t\t$value\n"
-      case ASTBoolConstant(_, value) => ("\t" * tabLevel) + "ldc\t\t" + (if (value) 1 else 0) + "\n"
+      case ASTIntConstant(_, value) => ("\t" * tabLevel) + s"ldc\t\t0x${value.toHexString}\n"
+      case ASTBoolConstant(_, value) => ("\t" * tabLevel) + "ldc\t\t0x" + (if (value) 1 else 0) + "\n"
      }
     case s: Stmt => ("\t" * tabLevel) + s".line ${s.pos.line}\n" + (s match {
       case PrintStmt(exprs, _) => exprs match {
