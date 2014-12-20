bdea85784c6a0e210bb41fc58fc57a324a039cce
Hawk Weisman <hawk@meteorcodelabs.com>
2014-12-20 15:10:53 -0500

    New compiler with actual options

--- src/main/scala/decaf/Compiler.scala
+++ src/main/scala/decaf/Compiler.scala
@@ -9,6 +9,9 @@
 \*                                        */
 
 package decaf
+
+import java.nio.file.{Files, Paths}
+
 import decaf.frontend.{DecafSyntactical, DecafSemantic}
 import decaf.backend.JasminBackend
 import scala.io.Source
@@ -21,6 +24,7 @@ import scala.io.Source
  *
  * Created by hawk on 10/10/14.
  */
+/*
 object Compiler extends App {
   val parser = new DecafSyntactical
   val (source: String, fileName: Option[String]) = args match {
@@ -43,6 +47,54 @@ object Compiler extends App {
     case Nil => println(JasminBackend.compile(ast, fileName))
     case errors: List[Exception] => errors.foreach(System.err.println(_))
   }
+*/
+object Compiler {
+  protected val parser = new DecafSyntactical
+  protected val optParser = new scopt.OptionParser[Config]("dcc") {
+    head("dcc", "0.1")
+    arg[String]("<source>") required() action { (x, c) =>
+      c.copy(path = x)
+    } validate { x =>
+      val path = Paths get x
+      if (Files.exists(path) && Files.isReadable(path)) success
+      else failure("Please enter a Decaf source code file to compile.")
+    } text "Decaf source code file to compile"
+    opt[Unit]('a', "ast") action { (_, c) =>
+      c.copy(astMode = true)
+    } text "Print the AST rather than compiling"
+    help("help") text "prints this usage text"
+  }
 
-
+  def main(args: Array[String]): Unit = optParser parse(args, Config()) match {
+    case Some(conf: Config) =>
+      val (source: String, fileName: Option[String]) = (
+        Source
+          .fromFile(conf.path)
+          .mkString,
+        Some(
+          conf
+            .path
+            .split('/')
+            .last
+            .split('.')
+            .head
+          )
+        )
+      lazy val ast = parser.parse(source)
+      conf match {
+        case Config(_,true) =>
+          println(ast)
+        case Config(_,false) =>
+          lazy val (scopes, errors) = DecafSemantic.analyze(ast)
+          errors match {
+            case Nil => println(JasminBackend.compile(ast, fileName))
+            case errors: List[Exception] => errors.foreach(System.err.println(_))
+          }
+      }
+    case None => System.exit(1)
+  }
 }
+case class Config(
+                   path: String = ".",
+                   astMode: Boolean = false
+                   )
